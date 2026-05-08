978bb58f0a73d318e9f4beb2db1508720b42b079
exy <teslawiktor@gmail.com>
2026-05-08 23:18:49 +0200

    feat: Premium page

---
+++ frontend/app/[locale]/(academy)/premium/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function PremiumPage() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="font-serif">Premium</h1>
+    </div>
+  )
+}
