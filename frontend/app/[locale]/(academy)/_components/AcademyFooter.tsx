3618fe9bb93469e5babcfd48c0c93fc8edd9986d
exy <teslawiktor@gmail.com>
2026-05-08 23:17:59 +0200

    feat: add Footer, border-r, Download app

--- frontend/app/[locale]/(academy)/_components/AcademyFooter.tsx
+++ frontend/app/[locale]/(academy)/_components/AcademyFooter.tsx
@@ -1,16 +1,28 @@
 import Link from 'next/link'
+import { FiSmartphone } from 'react-icons/fi'
 
 export function AcademyFooter() {
   return (
     <footer className="border-foreground/10 flex items-center justify-between border-t p-4 font-sans opacity-50">
       <div>Copyright © {new Date().getFullYear()} The Frontend Journal</div>
-      <div className="flex gap-4">
-        <Link href="/help" className="hover:underline">
-          Help Center
-        </Link>
-        <Link href="/terms" className="hover:underline">
-          Terms
-        </Link>
+      <div className="flex gap-2">
+        <div className="border-foreground20 border-r">
+          <Link href="/help" className="flex items-center gap-1 pr-2 hover:underline">
+            <FiSmartphone className="text-md" />
+            Download App
+          </Link>
+        </div>
+        <div className="border-foreground20 border-r">
+          <Link href="/help" className="pr-2 hover:underline">
+            Help Center
+          </Link>
+        </div>
+        <div className="border-foreground20 border-r">
+          <Link href="/terms" className="pr-2 hover:underline">
+            Terms
+          </Link>
+        </div>
+
         <Link href="/privacy" className="hover:underline">
           Privacy Policy
         </Link>
