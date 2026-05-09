dcd66eafc2d65389e86cd13cf2aa2cc813ce4f53
exy <teslawiktor@gmail.com>
2026-05-09 16:31:01 +0200

    feat: add placeholder Redeem page

---
+++ frontend/app/[locale]/(academy)/store/redeem/page.tsx
@@ -0,0 +1,7 @@
+export default function RedeemPage() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <h1 className="font-serif">Redeem</h1>
+    </div>
+  )
+}
