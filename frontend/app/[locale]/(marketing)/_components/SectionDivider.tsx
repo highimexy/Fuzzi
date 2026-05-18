abc93739115f398d9da8f96925f546fe3557add3
exy <teslawiktor@gmail.com>
2026-05-18 20:58:56 +0200

    fix: add z-10 to section divider to prevent overlap issues

--- frontend/app/[locale]/(marketing)/_components/SectionDivider.tsx
+++ frontend/app/[locale]/(marketing)/_components/SectionDivider.tsx
@@ -2,7 +2,7 @@
 
 export function SectionDivider() {
   return (
-    <div className="border-foreground/10 relative w-full border-y">
+    <div className="border-foreground/10 relative z-10 w-full border-y">
       {/* Kratka na całą szerokość */}
       <div className="relative h-4 w-full overflow-hidden">
         <div
@@ -17,7 +17,6 @@ export function SectionDivider() {
         />
       </div>
 
-      {/* Diamenciki - ten wrapper wymusza identyczną szerokość co Container */}
       <div className="pointer-events-none absolute inset-0 mx-auto w-[calc(100%-2rem)] max-w-360 md:w-[calc(100%-4rem)] lg:w-[calc(100%-8rem)]">
         {/* Górne */}
         <div className="bg-accent absolute top-0 left-0 h-1.5 w-1.5 -translate-x-1/2 -translate-y-1/2 rotate-45" />
